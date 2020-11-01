84434095fd771e1511967b626c80f909c4d15fab
Krzysztof <kzajac1997@gmail.com>
2020-11-01 17:09:36 +0100

    Update create tables

--- scripts/sql/create_tables.sql
+++ scripts/sql/create_tables.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Airline (
     [AirlineId] [smallint] PRIMARY KEY NOT NULL UNIQUE,
-    [Name] [varchar] NOT NULL UNIQUE
+    [Name] [varchar] NOT NULL UNIQUE,
+    [AirlineCode] [char(3)] NOT NULL UNIQUE
 );
 
 CREATE TABLE Airport (
@@ -13,30 +14,30 @@ CREATE TABLE Airport (
 
 CREATE TABLE Route (
     [RouteId] [int] PRIMARY KEY NOT NULL UNIQUE,
-    [ArrivalAirportId] [smallint] NOT NULL UNIQUE,
+    [DepartureAirportId] [smallint] NOT NULL UNIQUE,
     [DepartureAirportId] [smallint] NOT NULL UNIQUE,
     [ArrivalDateTime] [date],
     [DepartureDateTime] [date],
 
-    CONSTRAINT ArrivalAirportFk
-        FOREIGN KEY (ArrivalAirportId) REFERENCES Airport(AirportId),
-
     CONSTRAINT DepartureAirportFk
-        FOREIGN KEY (DepartureAirportId) REFERENCES Airport(AirportId)
+        FOREIGN KEY (DepartureAirportId) REFERENCES Airport(AirportId),
+
+    CONSTRAINT ArrivalAirportFk
+        FOREIGN KEY (ArrivalAirportId) REFERENCES Airport(AirportId)
 );
 
 
 CREATE TABLE Flight (
     [FlightId] [int] PRIMARY KEY NOT NULL UNIQUE,
-    [AirportId] [smallint] NOT NULL UNIQUE,
+    [AirlineId] [smallint] NOT NULL UNIQUE,
     [RouteId] [int] NOT NULL UNIQUE,
-    [FlightCode] [char(6)] NOT NULL UNIQUE,
+    [FlightCode] [char(12)] NOT NULL UNIQUE,
     [Duration] [time],
     [Passengers] [smallint],
-    [International] [bit],
+    [Intercontinental] [bit],
 
-    CONSTRAINT AirportFk
-        FOREIGN KEY (AirportId) REFERENCES Airport(AirportId),
+    CONSTRAINT AirlineFk
+        FOREIGN KEY (AirlineId) REFERENCES Airline(AirlineId),
 
     CONSTRAINT RouteFk
         FOREIGN KEY (RouteId) REFERENCES Route(RouteId)
