039620455beb42ff57602884ba6ceb98565d5fe5
Krzysztof <kzajac1997@gmail.com>
2020-11-28 13:20:17 +0100

    Update set_up and README.md file

--- scripts/shell/set_up.sh
+++ scripts/shell/set_up.sh
@@ -13,7 +13,7 @@ echo "set up geo partitioning...."
 ./yugabyte/bin/yb-admin --master_addresses yb-master-n1 modify_table_placement_info ysql.flight_db flight_america local.america.america-1a 1
 
 echo "populating database..."
-./yugabyte/bin/ysqlsh -h -q yb-tserver-n1-europe -c "\c flight_db" -c "\i flight_db/scripts/sql/populate/airlines.sql"
-./yugabyte/bin/ysqlsh -h -q yb-tserver-n1-europe -c "\c flight_db" -c "\i flight_db/scripts/sql/populate/airports.sql"
-./yugabyte/bin/ysqlsh -h -q yb-tserver-n1-europe -c "\c flight_db" -c "\i flight_db/scripts/sql/populate/routes.sql"
-./yugabyte/bin/ysqlsh -h -q yb-tserver-n1-europe -c "\c flight_db" -c "\i flight_db/scripts/sql/populate/flights.sql"
+./yugabyte/bin/ysqlsh -h yb-tserver-n1-europe -q -c "\c flight_db" -c "\i flight_db/scripts/sql/populate/airlines.sql"
+./yugabyte/bin/ysqlsh -h yb-tserver-n1-europe -q -c "\c flight_db" -c "\i flight_db/scripts/sql/populate/airports.sql"
+./yugabyte/bin/ysqlsh -h yb-tserver-n1-europe -q -c "\c flight_db" -c "\i flight_db/scripts/sql/populate/routes.sql"
+./yugabyte/bin/ysqlsh -h yb-tserver-n1-europe -q -c "\c flight_db" -c "\i flight_db/scripts/sql/populate/flights.sql"
