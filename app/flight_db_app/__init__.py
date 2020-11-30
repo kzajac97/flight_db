5d2aa09dd503c364c5679a159f6acc8179827cf9
Krzysztof <kzajac1997@gmail.com>
2020-11-30 21:55:10 +0100

    Add paths to all required views

--- app/flight_db_app/__init__.py
+++ app/flight_db_app/__init__.py
@@ -6,7 +6,11 @@ def main(global_config, **settings):
     config.include("pyramid_chameleon")  # for .pt templates
     # all URL paths
     config.add_route("home", "/")
-    config.add_route("hello", "/howdy")
+    config.add_route("register_flight", "/register_flight")
+    config.add_route("report_flight", "/report_flight")
+    config.add_route("flight_stats", "/flight_stats")
+    config.add_route("register_airport", "/register_airport")
+    config.add_route("register_airline", "/register_airline")
     # Path to directory with all static views
     config.add_static_view(name="static", path="flight_db_app:static")
     config.scan(".views")
