ee61bd520690d8ffb7e31b641b4dcc4269705798
Krzysztof <kzajac1997@gmail.com>
2020-11-30 21:16:07 +0100

    Add simple view for home url

--- app/flight_db_app/__init__.py
+++ app/flight_db_app/__init__.py
@@ -3,8 +3,12 @@ from pyramid.config import Configurator
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
-    config.include('pyramid_jinja2')
-    config.add_route('home', '/')
-    config.add_route('hello', '/howdy')
-    config.scan('.views')
+    config.include("pyramid_chameleon")  # for .pt templates
+    # all URL paths
+    config.add_route("home", "/")
+    config.add_route("hello", "/howdy")
+    # Path to directory with all static views
+    config.add_static_view(name="static", path="flight_db_app:static")
+    config.scan(".views")
+
     return config.make_wsgi_app()
--- app/flight_db_app/views.py
+++ app/flight_db_app/views.py
@@ -4,15 +4,15 @@ from pyramid.view import (
     )
 
 
-@view_defaults(renderer='templates/home.pt')
+@view_defaults(renderer="templates/home.pt")
 class TutorialViews:
     def __init__(self, request):
         self.request = request
 
-    @view_config(route_name='home')
+    @view_config(route_name="home")
     def home(self):
-        return {'name': 'Home View'}
+        return {"name": "Home View"}
 
-    @view_config(route_name='hello')
+    @view_config(route_name="hello")
     def hello(self):
-        return {'name': 'Hello View'}
+        return {"name": "Hello View"}
